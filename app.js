733714b3d5f053243af675b88b083ee5cf36d21c
kghorbanzadeh <kamandg1@educbe.ca>
2021-05-06 12:51:09 -0600

    updated version

--- app.js
+++ app.js
@@ -1,34 +1,26 @@
-const api_url_random = "https://api.adviceslip.com/advice";
-
-async function randomAdvice(){
-    const response = await fetch(api_url_random);
-    var data = await response.json();
-    document.getElementById('advice').innerHTML = data.slip.advice;
-}
-
-const changeAdvice = {
-    methods: {
-        onClick(){
-            this.$emit(randomAdvice())
-        }
-    }
-} 
+const api_url_random = "https://api.adviceslip.com/advice"
 
 const app = Vue.createApp({
     data() {
         return{
-            advice: randomAdvice(),
+            advice: "",
+            url: "https://api.adviceslip.com/advice"
             }
-        },
-        components:{
-            changeAdvice
         },
         methods:{
-            onClickModel(){
-               randomAdvice()
+            async randomAdvice(){
+                const response = await fetch(api_url_random);
+                var data = await response.json();
+                this.advice = data.slip.advice
+                //document.getElementById('advice').innerHTML = data.slip.advice;
             }
-        }
-    })
+        }, 
+        mounted: function(){
+            this.$nextTick(function(){
+                    this.randomAdvice()
+        })
+    }
+})
 
 app.mount('#adviceGiver');
 
